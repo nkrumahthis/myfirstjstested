ae62a13d48555bb5a4c4dc7dd4dd416930df9c88
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-28 17:38:54 +0000

    greater expectations

--- textUtilities.js
+++ textUtilities.js
@@ -3,7 +3,14 @@ var expect = require('chai').expect;
 expect(true).to.be.true;
 
 function titleCase(title){
-    return title;
+    var words = title.split(' ');
+    var titleCasedWords = words.map((word)=>{
+        return word[0].toUpperCase() + word.substring(1);
+    });
+    return titleCasedWords.join(' ');
 }
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A')
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
+expect(titleCase('vertigo')).to.equal('Vertigo');
