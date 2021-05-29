a71d6db5e8ef7d65135617b71863c0ddb4d9b217
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-29 19:27:16 +0000

    can find multiple ships

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -8,8 +8,10 @@ function checkForShip(player, coordinates){
             return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1])
         })[0];
 
-        return (!shipPresent) ? false : true;
+        if (shipPresent) return true
     }
+
+    return false
 
 }
 
