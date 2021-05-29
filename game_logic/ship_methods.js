957fe7598fe6a2ff9650baee4db90417af41c960
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-29 20:03:48 +0000

    fire function

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -8,7 +8,7 @@ function checkForShip(player, coordinates){
             return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1])
         })[0];
 
-        if (shipPresent) return true
+        if (shipPresent) return ship;
     }
 
     return false
@@ -19,5 +19,13 @@ function damageShip(ship, coordinates){
     ship.damage.push(coordinates)
 }
 
+function fire(player, coordinates){
+    var ship = checkForShip(player, coordinates);
+    if(ship){
+        damageShip(ship, coordinates)
+    }
+}
+
 module.exports.checkForShip = checkForShip;
 module.exports.damageShip = damageShip;
+module.exports.fire = fire;
